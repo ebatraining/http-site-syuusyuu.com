90543179e26bf41573687e94487573f9cd3d2b77
田多みのり <minori@tm-mac.local>
2017-12-11 10:14:20 +0900

    (index) viewportを追加

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <meta charset="UTF-8">
 <meta name="keywords" content="サイト収集.com,サイト 一覧,サイト紹介,ホームページ 一覧,ホームページ,ホームページ紹介,サイトデザイン">
 <meta name="description" content="サイト収集.comでは様々なサイト(ホームページ)を収集し、ご紹介させていただいております。ご興味のあるサイトがございましたら、ぜひ各リンクをクリックしてみてください。">
+<meta name="viewport" content="width=device-width,initial-scale=1">
 <title>サイト収集.com</title>
 <link href="css/base.css" rel="stylesheet" type="text/css" media="all">
 <script type="text/javascript" src="js/like-button.js"></script>
@@ -42,7 +43,7 @@
 		<p class="like2"><img src="imges/like-bottom.gif" alt="お気に入り"></p>
 		<input type="button" rel="sidebar" value="お気に入りに登録" onClick="toFavorite(location.href,document.title);">
 	</div><!--like終わり-->
-	
+
 	<!--ブログ埋め込み始め-->
 	<div class="news">
 	<?php include( $_SERVER['DOCUMENT_ROOT'] . '/news.php'); ?>
@@ -89,60 +90,60 @@
 " target="_blank"><img src="imges/kojinhogo_off.jpg" alt="首都圏コンピューター技術株式会社"/></a><a href="https://mcea.co.jp/company/privacy
 " target="_blank">個人情報保護</a>
 <span>首都圏コンピュータ技術者株式会社はIT個人事業主の技術者の方々の仕事と生活をサポートする専門会社として社会に貢献し、また、プロエンジニアの社会的経済的地位の向上に努めています。</span></li>
-<li><a href="http://www.online-kensa.com/lp/			
-" target="_blank"><img src="imges/hinin_off.jpg" alt="OES"/></a><a href="http://www.online-kensa.com/lp/			
+<li><a href="http://www.online-kensa.com/lp/
+" target="_blank"><img src="imges/hinin_off.jpg" alt="OES"/></a><a href="http://www.online-kensa.com/lp/
 " target="_blank">男性避妊</a>
 <span>OESは国内で初めて郵送による精子検査を実現いたしました。驚きの低価格で不妊治療をお手伝いさせていただき、多くの方にご好評いただいております。</span></li>
-<li><a href="http://mhageikumou.com/		
-" target="_blank"><img src="imges/emuji_off.jpg" alt="M字ハゲ治療薬"/></a><a href="http://mhageikumou.com/		
+<li><a href="http://mhageikumou.com/
+" target="_blank"><img src="imges/emuji_off.jpg" alt="M字ハゲ治療薬"/></a><a href="http://mhageikumou.com/
 " target="_blank">M字はげ</a>
 <span>頭皮のお悩みをお持ちのお客様から多くの支持をいただた商品のご案内ページです。M字はげ回復育毛剤ランキングを掲載し、その他詳細も記載させていただきました。</span></li>
-<li><a href="http://www.kirakuza.jp/		
-" target="_blank"><img src="imges/kirakuza_off.jpg" alt="気楽座"/></a><a href="http://www.kirakuza.jp/		
+<li><a href="http://www.kirakuza.jp/
+" target="_blank"><img src="imges/kirakuza_off.jpg" alt="気楽座"/></a><a href="http://www.kirakuza.jp/
 " target="_blank">肩こり 解消 グッズ</a>
 <span>気楽座は「貴陽石」を使用したアクセサリーを多数取り扱っております。疲労回復、リラックス効果等「貴陽石」のパワーを併せてご紹介しております。</span></li>
-<li><a href="http://kensetsu.kyokadaikou.com/			
- " target="_blank"><img src="imges/kensetu_off.jpg" alt="建設業許可代行"/></a><a href="http://kensetsu.kyokadaikou.com/			
+<li><a href="http://kensetsu.kyokadaikou.com/
+ " target="_blank"><img src="imges/kensetu_off.jpg" alt="建設業許可代行"/></a><a href="http://kensetsu.kyokadaikou.com/
  " target="_blank">建設業許可 | 建設業許可代行</a>
  <span>建設業許可取得をお考えの方を対象に代行のサービスをご提案致します。専門の行政書士が迅速・丁寧に対応しお客様のご満足をいただいております。</span></li>
-<li><a href="http://sis7shop.ca.shopserve.jp/lp/			
-" target="_blank"><img src="imges/kourai_off.jpg" alt="高麗人参サプリ"/></a><a href="http://sis7shop.ca.shopserve.jp/lp/			
+<li><a href="http://sis7shop.ca.shopserve.jp/lp/
+" target="_blank"><img src="imges/kourai_off.jpg" alt="高麗人参サプリ"/></a><a href="http://sis7shop.ca.shopserve.jp/lp/
 " target="_blank">高麗人参 サプリ</a>
 <span>疲労、ダイエット等日々の生活で健康面を気にかけておられる方にオススメの「高麗人参」。どなたでも接種しやすいようにサプリメントの形でご提供致します。</span></li>
 <li><a href="http://www.gelnail-oroshitonya.com/lp/
 " target="_blank"><img src="imges/gno_off.jpg" alt="GNO"/></a><a href="http://www.gelnail-oroshitonya.com/lp/
 " target="_blank">ジェルネイルオフ</a>
 <span>GNOからジュエルネイルを落とす時のお悩みの方へ「キュートオフパッド」のご紹介です。爪が傷ついてしまう、サロンに行く時間がない等のお悩みを解消いたいします。</span></li>
-<li><a href="http://h-kyoritsu.com/	
-" target="_blank"><img src="imges/hounan_off.jpg" alt="方南町薬局"/></a><a href="http://h-kyoritsu.com/	
+<li><a href="http://h-kyoritsu.com/
+" target="_blank"><img src="imges/hounan_off.jpg" alt="方南町薬局"/></a><a href="http://h-kyoritsu.com/
 " target="_blank">方南町 薬局</a>
 <span>方南町薬局は一人ひとりのお客様を大切に！をモットーにあなたのかかりつけ薬局を目指しています。経験豊かなスタッフが親身に対応いたします。</span></li>
-<li><a href="http://www.bestgame.jp/		
-" target="_blank"><img src="imges/best_off.jpg" alt="BESTGAME"/></a><a href="http://www.bestgame.jp/		
+<li><a href="http://www.bestgame.jp/
+" target="_blank"><img src="imges/best_off.jpg" alt="BESTGAME"/></a><a href="http://www.bestgame.jp/
 " target="_blank">ソーシャルゲーム ランキング | スマホゲーム ランキング</a>
 <span>BEST GAMEでは大人も子どもも楽しめるソーシャルゲームをランキング形式でご紹介。あなたにピッタリのコンテンツがきっと見つかります。</span></li>
-<li><a href="http://buzz-game.jp/	
-" target="_blank"><img src="imges/play_off.jpg" alt="PLAYREPORT"/></a><a href="http://buzz-game.jp/	
+<li><a href="http://buzz-game.jp/
+" target="_blank"><img src="imges/play_off.jpg" alt="PLAYREPORT"/></a><a href="http://buzz-game.jp/
 " target="_blank">ゲーム　攻略 | ゲーム　やってみた</a>
 <span>色んなジャンルのソーシャルゲームをユーザー目線で解説するために実際にプレイ。ブログ形式でリポートしていきます。ユル〜い目線でご覧ください。</span></li>
-<li><a href="http://www.medical-cook.co.jp/		
-" target="_blank"><img src="imges/medi_off.jpg" alt="メディカルクック宅食便"/></a><a href="http://www.medical-cook.co.jp/		
+<li><a href="http://www.medical-cook.co.jp/
+" target="_blank"><img src="imges/medi_off.jpg" alt="メディカルクック宅食便"/></a><a href="http://www.medical-cook.co.jp/
 " target="_blank">糖尿病 | 腎臓病食</a>
 <span>糖尿病・腎臓病などでお悩みの方にメディカルクック宅食便は安全な食材のみを厳選して栄養成分を十分考量し調整したお食事をご提供しております。</span></li>
-<li><a href="http://beauty.kirali.co.jp/search/			
-" target="_blank"><img src="imges/kirali_off.jpg" alt="KiRaLi Beauty"/></a><a href="http://beauty.kirali.co.jp/search/			
+<li><a href="http://beauty.kirali.co.jp/search/
+" target="_blank"><img src="imges/kirali_off.jpg" alt="KiRaLi Beauty"/></a><a href="http://beauty.kirali.co.jp/search/
 " target="_blank">表参道 美容室</a>
 <span>KiRaLi Beautyでは、東京の各エリアごとオススメの美容室・サロンをご紹介。自分に合う美容室・サロンを見つけてワンランク上のキレイを手に入れましょう。</span></li>
-<li><a href="http://nakanotani.com/		
-" target="_blank"><img src="imges/tomioka_off.jpg" alt="六方銀米通信販売"/></a><a href="http://nakanotani.com/		
+<li><a href="http://nakanotani.com/
+" target="_blank"><img src="imges/tomioka_off.jpg" alt="六方銀米通信販売"/></a><a href="http://nakanotani.com/
 " target="_blank">お米 通販</a>
 <span>豊岡市中谷村で栽培された「六方銀米」の通販サイト。厳選された栽培で皆様の食卓に安心と美味しさを提供致します。コウノトリが舞う自然豊かな環境で育まれた「六方銀米」です。</span></li>
 <li><a href="http://www.rental-o.com/
 " target="_blank"><img src="imges/rentaru_off.jpg" alt="レンタルオフィス.com"/></a><a href="http://www.rental-o.com/
 " target="_blank">レンタルオフィス</a>
 <span>レンタルオフィス.comでは東京・大阪をメインに全国から皆様のご希望にあうレンタルオフィスを検索出来るポータルサイトです。理想のオフィス作りに是非ご利用ください。</span></li>
-<li><a href="http://beauty-doll.net/	
-" target="_blank"><img src="imges/beauty_off.jpg" alt="Beauty doll"/></a><a href="http://beauty-doll.net/	
+<li><a href="http://beauty-doll.net/
+" target="_blank"><img src="imges/beauty_off.jpg" alt="Beauty doll"/></a><a href="http://beauty-doll.net/
 " target="_blank">美肌 秘訣</a>
 <span>美肌の秘訣なら、Beauty dollへ。クリアボーテはお肌の悩みの方の強い味方です。
 もっちりうるうるのお肌を手に入れるための栄養素が凝縮されたクリアボーテを是非。</span></li>
@@ -150,59 +151,59 @@
 " target="_blank"><img src="imges/misumirank_off.jpg" alt="MISUMIランキング"/></a><a href="http://accessory-ranking.com/
 " target="_blank">パワーストーン ブレスレット | 天然石 アクセサリー</a>
 <span>パワーストーン・アクセサリーのMISUMI様の人気商品ランキングページです。詳細も記載しておりますので皆様の参考にお役立て下さい。</span></li>
-<li><a href="http://sougou-ranking.com/			
-" target="_blank"><img src="imges/sougou_off.jpg" alt="総合ランキングサイト"/></a><a href="http://sougou-ranking.com/			
+<li><a href="http://sougou-ranking.com/
+" target="_blank"><img src="imges/sougou_off.jpg" alt="総合ランキングサイト"/></a><a href="http://sougou-ranking.com/
 " target="_blank">総合ランキングサイト | ランキングサイト</a>
 <span>様々なジャンルのランキングを掲載しております。皆様の生活にお役立ていただけるよう厳選したサイトをご紹介しております。</span></li>
-<li><a href="http://kaisya-syoukai.com/	
-" target="_blank"><img src="imges/kaisya_off.jpg" alt="会社紹介.com"/></a><a href="http://kaisya-syoukai.com/	
+<li><a href="http://kaisya-syoukai.com/
+" target="_blank"><img src="imges/kaisya_off.jpg" alt="会社紹介.com"/></a><a href="http://kaisya-syoukai.com/
 " target="_blank">会社紹介.com</a>
 <span>日本全国の良質な企業を集めたサイトです。日本全国にある企業の中でも素晴らしい事業・サービスを提供している会社をご紹介させていただいております。</span></li>
 <li><a href="http://syoukai-blog.com/
 " target="_blank"><img src="imges/syoukai_off.jpg" alt="紹介ブログ"/></a><a href="http://syoukai-blog.com/
 " target="_blank">紹介ブログ</a>
 <span>色々な情報をブログにして皆様に発信しております。日々の生活に役立つ情報からちょっとゆるい記事まで広い視野で日々更新しております。</span></li>
-<li><a href="http://jyouhou-hasshin.com/		
-" target="_blank"><img src="imges/jyohou_off.jpg" alt="情報発信.com"/></a><a href="http://jyouhou-hasshin.com/		
+<li><a href="http://jyouhou-hasshin.com/
+" target="_blank"><img src="imges/jyohou_off.jpg" alt="情報発信.com"/></a><a href="http://jyouhou-hasshin.com/
 " target="_blank">情報発信</a>
 <span>情報発信.comでは、様々な情報を発信していきます。企業の情報、ニュース、エンタメ、お笑い、恋バナ、身近な話し、どのようなこと情報として発信していきます。</span></li>
 <li><a href="http://katteni-kuchikomi.com/
 " target="_blank"><img src="imges/kutikomi_off.jpg" alt="勝手に口コミ紹介"/></a><a href="http://katteni-kuchikomi.com/
 " target="_blank">勝手に口コミ紹介 | 口コミ</a>
 <span>勝手に口コミ紹介とは、タイトル通りのサイトで、運営者が勝手に口コミをしていくサイトです。皆様がお気に召すサイトなどがありましたら、ぜひクリックしてみてください。</span></li>
-<li><a href="http://omowazu-syoukai.com/						
-" target="_blank"><img src="imges/omowazu_off.jpg" alt="思わず紹介.com"/></a><a href="http://omowazu-syoukai.com/						
+<li><a href="http://omowazu-syoukai.com/
+" target="_blank"><img src="imges/omowazu_off.jpg" alt="思わず紹介.com"/></a><a href="http://omowazu-syoukai.com/
 " target="_blank">思わず紹介 | 口コミ</a>
 <span>思わず紹介.comとは、全世界に存在するWebサイトの中から思わず紹介したくなる様々なホームページやサイトをご紹介させていただきます！気になるサイトなどがありましたら、ぜひクリックしてみてください。</span></li>
-<li><a href="http://site-syoukai.com/		
-" target="_blank"><img src="imges/site_off.jpg" alt="サイト紹介.com"/></a><a href="http://site-syoukai.com/		
+<li><a href="http://site-syoukai.com/
+" target="_blank"><img src="imges/site_off.jpg" alt="サイト紹介.com"/></a><a href="http://site-syoukai.com/
 " target="_blank">サイト紹介.com | 口コミ</a>
 <span>サイト紹介.comとは、様々なサイトを紹介するサイトです。新しいものから、数十年運営されている老舗サイトまで、様々なサイトをご紹介させていただきます。</span></li>
-<li><a href="http://good-site.biz/		
-" target="_blank"><img src="imges/good_off.jpg" alt="Goodサイト"/></a><a href="http://good-site.biz/		
+<li><a href="http://good-site.biz/
+" target="_blank"><img src="imges/good_off.jpg" alt="Goodサイト"/></a><a href="http://good-site.biz/
 " target="_blank">Goodサイト | 口コミ</a>
 <span>Goodサイトとは、良質なサイトを数億あるサイトの中から紹介していくサイトです。サイトの選定などは当社で行っておりますが、ご推薦、掲載希望のお客様がおられましたら、お気軽にご連絡くださいませ。</span></li>
 <li><a href="http://healthcare-div.weblike.jp/zemaitis/nyuusan/
 " target="_blank"><img src="imges/nyuusan_off.jpg" alt="ヘルスケアDIV乳酸菌サプリ"/></a><a href="http://healthcare-div.weblike.jp/zemaitis/nyuusan/
 " target="_blank">乳酸菌サプリ</a>
 <span>便秘、肌荒れ等でお悩みのお客様に乳酸菌サプリをご紹介しております。良質な乳酸菌を配合した商品を多数取り扱っておりますのでどなた様にもピッタリのサプリがきっと見つかります。</span></li>
-<li><a href="http://healthcare-div.weblike.jp/zemaitis/			
-" target="_blank"><img src="imges/jyousui_off.jpg" alt="ヘルスケアDIV浄水器"/></a><a href="http://healthcare-div.weblike.jp/zemaitis/			
+<li><a href="http://healthcare-div.weblike.jp/zemaitis/
+" target="_blank"><img src="imges/jyousui_off.jpg" alt="ヘルスケアDIV浄水器"/></a><a href="http://healthcare-div.weblike.jp/zemaitis/
 " target="_blank">浄水器カートリッジ</a>
 <span>毎日飲むお水においしさと安心を。浄水器カートリッジのことならヘルスケアDIVにお任せください。豊富な品揃えと格安商品で皆様の元へ上質のお水をご提供致します。</span></li>
-<li><a href="http://healthcare-div.weblike.jp/zemaitis/Lumbricus_rubellus/					
-" target="_blank"><img src="imges/ruku_off.jpg" alt="ルンブルクスルベルス"/></a><a href="http://healthcare-div.weblike.jp/zemaitis/Lumbricus_rubellus/					
+<li><a href="http://healthcare-div.weblike.jp/zemaitis/Lumbricus_rubellus/
+" target="_blank"><img src="imges/ruku_off.jpg" alt="ルンブルクスルベルス"/></a><a href="http://healthcare-div.weblike.jp/zemaitis/Lumbricus_rubellus/
 " target="_blank">ルンブルクスルベルス</a>
 <span>ルンブルクスルベルスは唯一、国の機関より特許として認められた製造方法により粉末化されたミミズ乾燥粉末を使い血液と血管の健康にお役立て下さい。</span></li>
-<li><a href="http://www.amamibussan.jp/lp/		
-" target="_blank"><img src="imges/amami_off.jpg" alt="奄美大島のお土産店"/></a><a href="http://www.amamibussan.jp/lp/		
+<li><a href="http://www.amamibussan.jp/lp/
+" target="_blank"><img src="imges/amami_off.jpg" alt="奄美大島のお土産店"/></a><a href="http://www.amamibussan.jp/lp/
 " target="_blank">黒砂糖</a>
 <span>長寿の島奄美のヘルシーな自然食品「黒砂糖」。奄美大島のお土産店では奄美大島の名産品を豊富に取り揃えております。奄美徳之島の黒砂糖は食べやすく、独特の香りからご好評いただいております。</span></li>
-<li><a href="http://d-onehome.blue/		
-" target="_blank"><img src="imges/d-one_off.jpg" alt="D-one home"/></a><a href="http://d-onehome.blue/		
+<li><a href="http://d-onehome.blue/
+" target="_blank"><img src="imges/d-one_off.jpg" alt="D-one home"/></a><a href="http://d-onehome.blue/
 " target="_blank">伊勢市 新築 | 伊勢市 デザイン 住宅</a><span>2014年度GOOD DESIGN AWARDを受賞したD-one homeではお客様のお希望に添えたデザイナーズ住宅をご提案いたします。設計士と共に作り上げる新築は満足度120%です。</span></li>
-<li><a href="http://catarina.rh.shopserve.jp/lpcasaines/	
-" target="_blank"><img src="imges/casa_off.jpg" alt="カーサイネス"/></a><a href="http://catarina.rh.shopserve.jp/lpcasaines/	
+<li><a href="http://catarina.rh.shopserve.jp/lpcasaines/
+" target="_blank"><img src="imges/casa_off.jpg" alt="カーサイネス"/></a><a href="http://catarina.rh.shopserve.jp/lpcasaines/
 " target="_blank">素敵な輸入インテリア雑貨 食器 | フランス雑貨 通販</a>
 <span>ヨーロッパ・アメリカ等世界各国から取り寄せたシャビーシックでエレガントな輸入雑貨を数多く取り揃えております。上質な大人の時間を演出するお手伝いをさせていただいております。</span></li>
 <li><a href="http://bf-daiichi.red/" target="_blank"><img src="imges/daiichi_off.jpg" alt="第一建設"></a>
